433a6106485f81df1f1d40fadb8f62756a2f720e
Lilia Rigumye <rigumyel@gmail.com>
2014-08-24 15:20:33 +0200

    modified makeCachedMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##  makeCacheMatrix() is a function that takes a 2*2 matrix as input, computes 
+## the inversed matrix and stores it in a variable called inversedmat 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(amat = matrix()) {
+  inversedmat <- NULL
+  set <- function(imat){
+     amat <<- imat
+     inversedmat <<- NULL
+  }
+  get <- function(){ amat }
+  setInverse <- function(solve){ inversedmat <<- solve(amat) }
+  getInverse <- function(){ inversedmat }
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
+## cacheSolve() is afunction that computes the inverse of a matrix passed to it ## as an argument, for the 1st time or retreives the cached inversed matrix if  ## it had been called previously with the same argument 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a, ...) {
+        ## Return a matrix that is the inverse of 'a'
+    inversedmat <- a$getInverse()
+    if(!is.null(inversedmat)){
+        message("getting cached data")
+	return(inversedmat)
+    }
+    data <-a$get()
+    inversedmat <- solve(data, ...)
+    a$setInverse(inversedmat)
+    inversedmat
 }
